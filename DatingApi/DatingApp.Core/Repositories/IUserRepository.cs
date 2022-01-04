207fb2bb9914cab57119a2f00a1b2700e6ab049d
Adnan <adnanalishair@gmail.com>
2022-01-04 22:54:38 +0500

    first commit

---
+++ DatingApi/DatingApp.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Employee.Core.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Core.Repositories
+{
+    public interface IUserRepository : IRepository<DatingApp.Core.Entities.User>
+    {
+        //custom operations here
+        Task<IEnumerable<DatingApp.Core.Entities.User>> GetEmployeeByLastName(string lastname);
+    }
+}
